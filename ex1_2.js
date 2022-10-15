dffb88441d0272d4355712eda81bb4315bfcb0e7
Thuytrang <19521037@gm.uit.edu.vn>
2022-10-15 10:11:47 +0700

    lab2_ex1

---
+++ ex1_2.js
@@ -0,0 +1,23 @@
+const copy_to_Clipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    const selected =
+      document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    if (selected) {
+      document.getSelection().removeAllRanges();
+      document.getSelection().addRange(selected);
+    }
+  };
+  copy_to_Clipboard('JavaScript basic - Exercises, Practice, Solution');
+  if (typeof window !== 'undefined') {
+    console.log('You are on the browser')
+  } else {
+    console.log('You are on the server')
+  }
---
+++ ex1_4.js
@@ -0,0 +1,12 @@
+const isValidJSON = obj => {
+    try {
+      JSON.parse(obj);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+  console.log(isValidJSON('{"name":"trang","age":20}'));
+  console.log(isValidJSON(null));
+  console.log(isValidJSON('{"name":"Adam",age:"20"}'));
+  
