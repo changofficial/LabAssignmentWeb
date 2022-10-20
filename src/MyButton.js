debc7b8ecb883932fab0ec336891a35a50e68b96
Thuytrang <19521037@gm.uit.edu.vn>
2022-10-20 15:13:53 +0700

    in class lab 3 ex1.2

--- src/MyButton.js
+++ src/MyButton.js
@@ -2,6 +2,7 @@ import * as React from "react";
 
 class MyButton extends React.Component{
     render() {
+        const {disabled, text } = this.props;
         return <button>{this.props.children}</button>;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import MyComponent from "./MyComponent";
+import MyButton from "./MyButton";
+import MyComponent from "./MyButton";
+import MyList from "./MyList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render (<MyComponent/>);
+const appState = {
+  text: "My Button",
+  disable:true,
+  items: ["First", "Second", "Third"],
+};
+
+function render(props) {
+  root.render (
+    <main>
+      <MyButton text={props.text} disable={props.disable} />
+      <MyList items = {props.items} />
+    </main>
+  );
+}
+render(appState);
+
+setTimeout(() => {
+  appState.disable = false;
+  appState.items.push("Fourth");
+
+  render(appState);
+}, 1000)
