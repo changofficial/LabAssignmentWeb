9de514c01f7f84eee51f39bdfb9b0abcc6198dce
Thuytrang <19521037@gm.uit.edu.vn>
2022-10-20 15:39:29 +0700

    in class lab 3 ex2.2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import * as React from "react";
-export default function App() {
-    const [name] = React.useState("Adam");
-    const [age] = React.useState(35);
 
-    return (
-        <>
-        <p>My name is {name}</p>
-        <p>My age is {age}</p>
-        </>
-    );
+function App() {
+    const [name,setName] = React.useState("Trang");
+    const [age, setAge]= React.useState(21);
+  return (
+    <>
+        <section>
+            <input value={name}
+                onChange = {(e) => setName(e.target.value)}>
+                
+            </input>
+            <p>My name is {name}</p>
+        </section>
+        <section>
+            <input type="number"
+                value={age}
+                onChange={(e)=> setAge(e.target.value)}>
+            </input>
+            <p>My age is {age}</p>
+        </section>
+    </>
+  )
 }
+
+export default App
