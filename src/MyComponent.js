f5d10428c227786e3569022b1eafb8faa4a14f95
Thuytrang <19521037@gm.uit.edu.vn>
2022-10-20 14:58:40 +0700

    in class lab 3 ex1.1

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,21 +1,26 @@
-
 import * as React from 'react';
 
 class MyComponent extends React.Component{
   state = {
-    first: false,
-    second: true
+    heading: "React Awesomesauce (Busy)",
+    content: "Loading...",
+  }
+
+  constructor() {
+    super();
+    setTimeout(() => {
+      this.setState({
+        heading: "React Awesomesauce",
+        content: "Done!",
+      })
+    }, 3000);
   }
 render() {
-  const { first, second } =  this.state;
+  const { heading, content } =  this.state;
   return (
     <main>
-      <section>
-        <button disabled={first}>First</button>
-      </section>
-      <section>
-        <button disabled={second}>Second</button>
-      </section>
+      <h1>{heading}</h1>
+      <p>{content}</p>
     </main>
   )
   }
