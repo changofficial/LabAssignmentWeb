38df004a5b577a62db1c6cca2f50c2551e185164
Thuytrang <19521037@gm.uit.edu.vn>
2022-10-20 15:31:51 +0700

    in class lab 3 ex2.1

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import MyButton from "./MyButton";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-function render({second}) {
-  root.render (
-    <main>
-      <MyButton/>
-      <MyButton text={second.text} disabled={second.disabled} />
-    </main>
-  );
-}
-render({
-  second: {
-    text:"Second Button",
-    disabled:true,
-  },
-});
-
+root.render(<App/>);
