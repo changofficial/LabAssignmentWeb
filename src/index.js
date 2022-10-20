7c13257e3e6cf274c3c3043a6072ec8ac0c6dbbc
Thuytrang <19521037@gm.uit.edu.vn>
2022-10-20 14:50:55 +0700

    in class lab 3 ex1.0

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import MyComponent from "./MyComponent";
 
-
-const array = ["First", "Second", "Third"];
-
-const object = {
-  first: 1,
-  second:2,
-  third:3,
-};
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <section>
-    <h1>Array</h1>
-    <ul>
-      {array.map((i) => (<li key={i}>{i}</li>))}
-    </ul>
-
-    <h1>Object</h1>
-    <ul>
-      {Object.keys(object).map((i) =>(
-        <li Key={i}>
-          <strong>{i}: </strong>
-          {object[i]}
-        </li>
-      ))}
-    </ul>
-  </section>
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render (<MyComponent/>);
